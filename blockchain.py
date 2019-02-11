52704d42cbc768b005cd3fda959acae10b4fe253
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-02-12 06:17:19 +0900

    [Add] create blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,66 @@
 # coding: UTF-8
 
+import hashlib
+import json
+from time import time
+
 class Blokchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+
+    # ジェネシスブロックを作る
+    self.new_block(previous_hash=1, proof=100)
   
-  def new_block(self):
-    # 新しいブロックを作りチェーンを追加する
+  """
+  ブロックチェーンに新しいブロックを作る
+  :param proof: <int> プルーフ・オブ・ワークアルゴリズムから得られるプルーフ
+  :param previous_hash: (オプション) <str> 前のブロックのハッシュ
+  :return: <dict> 新しいブロック
+  """
+  def new_block(self, proof, previous_hash=None):
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash or self.hash(self.chain[-1]),
+    }
+
+    # 現在のトランザクションリストをリセット
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
+
+  """
+  次に採掘されるブロックに加える新しいトランザクションを作る
+  :param sender: <str> 送信者のアドレス
+  :param recipient: <str> 受信者のアドレス
+  :param amount: <int> 量
+  :return: <int> このトランザクションを含むブロックのアドレス
+  """
+  def new_transcation(self, sender, recipient, amount):
+    self.current_transactions.append({
+      'sender': sender,
+      'recipient': recipient,
+      'amount': amount,
+    })
+
+    return self.last_block['index'] + 1
   
-  def new_transcation(self):
-    # 新しいトランザクションをリストに加える
-  
+  """
+  ブロックの SHA-256 ハッシュを作る
+  :param block: <dict> ブロック
+  :return <str>
+  """
   @staticmethod
   def hash(block):
-    # ブロックをハッシュ化する
-    pass
+    # 必ずディクショナリ（辞書型のオブジェクト）がソートされている必要がある。そうでないと一貫性のないhashとなってしまう
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
   
   @property
   def last_block(self):
     # チェーンの最後のブロックをリターンする
-    pass
+    return self.chain[-1]
