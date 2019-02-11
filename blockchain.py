acc57a4bc18cadab406ab792b5e56c0a5acd00a6
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-02-12 05:35:41 +0900

    [Add] first commit

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+# coding: UTF-8
+
+class Blokchain(object):
+  def __init__(self):
+    self.chain = []
+    self.current_transactions = []
+  
+  def new_block(self):
+    # 新しいブロックを作りチェーンを追加する
+  
+  def new_transcation(self):
+    # 新しいトランザクションをリストに加える
+  
+  @staticmethod
+  def hash(block):
+    # ブロックをハッシュ化する
+    pass
+  
+  @property
+  def last_block(self):
+    # チェーンの最後のブロックをリターンする
+    pass
